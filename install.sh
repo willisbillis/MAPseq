597163f696a97df99ddc8b2b12eb4a6b380ded9c
willisbillis <elliott.williams14@gmail.com>
2024-01-11 18:58:01 -0500

    updates

--- install.sh
+++ install.sh
@@ -36,6 +36,19 @@ if [ -z "$(echo $PATH | grep -o ~/.local/bin)" ]; then
 fi
 echo "Appending 'source $PWD/src/create_run.sh' to ~/.bashrc to add bash functions in this repository..."
 chmod +x src/create_run.sh
-echo "source $PWD/src/create_run.sh" >> ~/.bashrc
+echo "MAPSEQ_REPO_PATH=$PWD" >> ~/.bashrc
+echo "source $MAPSEQ_REPO_PATH/src/create_run.sh" >> ~/.bashrc
 source ~/.bashrc
-echo "Install complete!"
+echo "Creating dummy project config file in LRAXXX_template for example..."
+cat $MAPSEQ_REPO_PATH/src/project_config_header.txt >> $1/project_config.txt
+echo "PROJECT_NAME=LRAXXX" >> $1/project_config.txt
+echo "PROJECT_PATH=$PWD/LRAXXX" >> $1/project_config.txt
+cat $MAPSEQ_REPO_PATH/src/project_config_body.txt >> $1/project_config.txt
+
+echo -e "Install complete! \n\n"
+echo "To create a new directory for a single run, use the command below with an appropriately named run name."
+echo -e "\ncreate_run <RUN_NAME>\n"
+echo -e "This directory (except for configuration and reference files) will mirror the local repository version of this pipeline.\n"
+echo "To create a new directory for aggregating all completed runs, use the command below with an appropriately named run name."
+echo -e "\ncreate_aggr_run <AGGR_RUN_NAME>\n"
+echo "This directory (except for configuration and reference files) will mirror the local repository version of this pipeline, too."
--- src/create_run.sh
+++ src/create_run.sh
@@ -2,7 +2,7 @@
 
 function create_run() {
     if [ $# -ne 1 ]; then
-        echo "Error: Exactly 1 arguments required!"
+        echo "Error: Exactly 1 argument required!"
         return 1
     fi
     if [ -z "$1" ]; then
@@ -10,7 +10,21 @@ function create_run() {
         return 1
     fi
 
-    ln -s LRAXXX_template $1
+    ln -s $MAPSEQ_REPO_PATH/LRAXXX_template $1
+    unlink $1/data/LRAXXX.ATAC.sampleManifest.csv
+    touch $1/data/$1.ATAC.sampleManifest.csv
+    unlink $1/data/LRAXXX.RNA.sampleManifest.csv
+    touch $1/data/$1.RNA.sampleManifest.csv
+    unlink $1/pipeline/RNA.FB.BCR/TSC_feature_ref.csv
+    cp $MAPSEQ_REPO_PATH/LRAXXX_template/pipeline/RNA.FB.BCR/TSC_feature_ref.csv $1/pipeline/RNA.FB.BCR
+    unlink $1/pipeline/ATAC.ASAP/HTOB_feature_ref.csv
+    cp $MAPSEQ_REPO_PATH/LRAXXX_template/pipeline/ATAC.ASAP/HTOB_feature_ref.csv $1/pipeline/ATAC.ASAP
+
+    unlink $1/project_config.txt
+    cat $MAPSEQ_REPO_PATH/src/project_config_header.txt >> $1/project_config.txt
+    echo "PROJECT_NAME=$1" >> $1/project_config.txt
+    echo "PROJECT_PATH=$PWD/$1" >> $1/project_config.txt
+    cat $MAPSEQ_REPO_PATH/src/project_config_body.txt >> $1/project_config.txt
 }
 
 function create_aggr_run() {
@@ -23,5 +37,6 @@ function create_aggr_run() {
         return 1
     fi
 
-    ln -s LRA_all_template $1
+    ln -s $MAPSEQ_REPO_PATH/LRA_all_template $1
+
 }
