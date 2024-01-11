39d65f976c02de6470435243c79001abf74aefa9
willisbillis <elliott.williams14@gmail.com>
2024-01-11 10:54:21 -0500

    fix sample array

--- LRAXXX_template/pipeline/RNA.FB.BCR/run_cellranger_RNA.FB.BCR.sh
+++ LRAXXX_template/pipeline/RNA.FB.BCR/run_cellranger_RNA.FB.BCR.sh
@@ -18,11 +18,10 @@ OUTPUT_FILE=$OUTPUT_DIR/cellranger_rna.fb.bcr_mapping.log
 ################################################################################
 mkdir -p $OUTPUT_DIR
 sample_name_col=$(cut -d, -f2 $PROJECT_PATH/data/${PROJECT_NAME}.RNA.sampleManifest.csv)
-sample_names=$(printf -- '%s\n' "${sample_name_col[@]}" | grep -v Sample | uniq)
-echo $sample_names # verbose for testing
-bcr_samples=$(printf -- '%s\n' "${sample_names[@]}" | grep .*${BCR_NAMING_ID}.*)
-rna_samples=$(printf -- '%s\n' "${sample_names[@]}" | grep .*${GEX_NAMING_ID}.*)
-echo $rna_samples # verbose for testing
+sample_names=$(printf -- '%s ' "${sample_name_col[@]}" | grep -v Sample | uniq)
+
+bcr_samples=$(printf -- '%s ' "${sample_names[@]}" | grep .*${BCR_NAMING_ID}.*)
+rna_samples=$(printf -- '%s ' "${sample_names[@]}" | grep .*${GEX_NAMING_ID}.*)
 
 CR_version=$(cellranger --version | grep -Po '(?<=cellranger-)[^;]+')
 echo "$(date) Running Cell Ranger version $CR_version using binary $(which cellranger)" >> $OUTPUT_FILE
