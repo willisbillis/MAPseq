9788c2dd61b17ffbbe896a113a4383623cdbd310
willisbillis <elliott.williams14@gmail.com>
2024-01-11 19:01:44 -0500

    add comment for reader at install

--- install.sh
+++ install.sh
@@ -36,9 +36,11 @@ if [ -z "$(echo $PATH | grep -o ~/.local/bin)" ]; then
 fi
 echo "Appending 'source $PWD/src/create_run.sh' to ~/.bashrc to add bash functions in this repository..."
 chmod +x src/create_run.sh
+echo -e "\n# MAPSEQ INSTALLATION EDITS - DO NOT MANUALLY CHANGE (Installed at $(date))" >> ~/.bashrc
 echo "MAPSEQ_REPO_PATH=$PWD" >> ~/.bashrc
 echo "source $MAPSEQ_REPO_PATH/src/create_run.sh" >> ~/.bashrc
 source ~/.bashrc
+
 echo "Creating dummy project config file in LRAXXX_template for example..."
 cat $MAPSEQ_REPO_PATH/src/project_config_header.txt >> $1/project_config.txt
 echo "PROJECT_NAME=LRAXXX" >> $1/project_config.txt
