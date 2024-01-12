0e92a17d34b009dcd0758634ebe7fb654de8c516
willisbillis <elliott.williams14@gmail.com>
2024-01-11 19:17:18 -0500

    wrong paths

--- install.sh
+++ install.sh
@@ -36,15 +36,15 @@ if [ -z "$(echo $PATH | grep -o ~/.local/bin)" ]; then
 fi
 echo "Appending 'source $PWD/src/create_run.sh' to ~/.bashrc to add bash functions in this repository..."
 echo -e "\n# MAPSEQ INSTALLATION EDITS - DO NOT MANUALLY CHANGE (Installed at $(date))" >> ~/.bashrc
-echo "MAPSEQ_REPO_PATH=$PWD" >> ~/.bashrc
+echo "export MAPSEQ_REPO_PATH=$PWD" >> ~/.bashrc
 echo "source $MAPSEQ_REPO_PATH/src/create_run.sh" >> ~/.bashrc
 source ~/.bashrc
 
 echo "Creating dummy project config file in LRAXXX_template for example..."
-cat $MAPSEQ_REPO_PATH/src/project_config_header.txt >> $1/project_config.txt
+cat $MAPSEQ_REPO_PATH/src/project_config_header.txt >> $MAPSEQ_REPO_PATH/LRAXXX_template/project_config.txt
 echo "PROJECT_NAME=LRAXXX" >> $1/project_config.txt
 echo "PROJECT_PATH=$PWD/LRAXXX" >> $1/project_config.txt
-cat $MAPSEQ_REPO_PATH/src/project_config_body.txt >> $1/project_config.txt
+cat $MAPSEQ_REPO_PATH/src/project_config_body.txt >> $MAPSEQ_REPO_PATH/LRAXXX_template/project_config.txt
 
 echo -e "Installation complete! \n\n"
 echo "To create a new directory for a single run, use the command below with an appropriately titled run name."
