bae21fcdc9a1c76491d4e1a0f09bcb5a227c3d69
willisbillis <elliott.williams14@gmail.com>
2024-01-16 11:00:07 -0500

    first go at updating

--- src/LRA_all_template/pipeline/ATAC.ASAP/ATAC/run_cellranger.aggr_atac.sh
+++ src/LRA_all_template/pipeline/ATAC.ASAP/ATAC/run_cellranger.aggr_atac.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
-PROJECT_PATH=/home/boss_lab/Projects/Scharer_sc/LRA.MAPseq
-PROJECT_NAME=LRA_all
-REF_PATH=/home/boss_lab/Apps/genomes/cellranger-ATAC2/hg38
+#
+# run_cellranger.aggr_atac.sh - written by MEW (https://github.com/willisbillis) Jan 2024
+# This script runs the cellranger-atac aggr pipeline formatted for the ATAC + ASAP
+# side of the LRA MAPseq project started in 2023.
+#
+# NOTICE: At this point, the user has run through all steps in at least two
+#     ATAC + ASAP runs and is ready to aggregate the runs for a single project.
+################################################################################
+# Import all the global variables for this project
+source ../../project_config.txt
+
+# Set all the local variables for this pipeline
+SAMPLES_ARRAY=($(ls -d $PROJECT_PATH/*/pipeline/ATAC.ASAP/ATAC/*/))
 OUTPUT_DIR=$PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP/ATAC
-OUTPUT_FILE=$OUTPUT_DIR/cellranger_mapping.log
-
-MEM=128
-NCPU=32
-
+OUTPUT_FILE=$OUTPUT_DIR/cellranger_atac_aggr.log
+AGGR_CSV=$OUTPUT_DIR/aggr.csv
 ################################################################################
 mkdir -p $OUTPUT_DIR
-AGGR_CSV=$OUTPUT_DIR/aggr.csv
+cd $OUTPUT_DIR
 
 CR_version=$(cellranger-atac --version | grep -Po '(?<=cellranger-atac-)[^;]+')
 echo "$(date) Running Cell Ranger ATAC version $CR_version using binary $(which cellranger-atac)" >> $OUTPUT_FILE
 
-cd $OUTPUT_DIR
+## Create aggregation csv
 printf '%s\n' library_id fragments cells | paste -sd ',' >> $AGGR_CSV
-for sample_path in $(ls -d $PROJECT_PATH/LRA0*/pipeline/ATAC.ASAP/ATAC/*/)
-do
-if [[ $sample_path != *"_aggr"* ]]; then
+for sample_path in "${SAMPLES_ARRAY[@]}"; do
   sample_name=$(basename $sample_path)
-  fragments_file=$(realpath $sample_path)/outs/fragments.tsv.gz
+  fragments_file=$sample_pat/outs/fragments.tsv.gz
   cells_file=$(realpath $sample_path)/outs/singlecell.csv
   printf '%s\n' $sample_name $fragments_file $cells_file | paste -sd ',' >> $AGGR_CSV
-fi
 done
 
+## Run aggregation pipeline
 echo "$(date) Running sample aggregation..." >> $OUTPUT_FILE
-cellranger-atac aggr --id=${PROJECT_NAME}_aggr --csv $AGGR_CSV --reference $REF_PATH --localcores=$NCPU --localmem=$MEM
+cellranger-atac aggr --id=${PROJECT_NAME}_aggr \
+  --csv $AGGR_CSV --reference $ATAC_REF_PATH --normalize none \
+  --localcores=$NCPU --localmem=$MEM
