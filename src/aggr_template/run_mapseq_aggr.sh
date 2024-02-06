1d219f8aefe24896731aca644cf4419b55c27487
willisbillis <elliott.williams14@gmail.com>
2024-02-06 15:05:49 -0500

    automate aggr

---
+++ src/aggr_template/run_mapseq_aggr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# run_mapseq_aggr.sh - written by MEW (https://github.com/willisbillis) Feb 2023
+# This script runs the MAPseq aggregation pipeline.
+
+# NOTICE: At this point, the user has run through all steps in at least two
+#       RNA and ATAC runs and is ready to aggregate the runs for a single 
+#       project. Additionally, the user has set and checked all variables
+#       in the project_config.txt and populated the sample hashtag
+#       sheets with the correct hashtags for demultiplexing the pools.
+################################################################################
+# Import all the global variables for this project
+source ./project_config.txt
+################################################################################
+# TODO: add unit tests here
+
+# run cellranger aggr on the RNA+FB samples
+cd $PROJECT_PATH/pipeline/RNA.FB.VDJ && $PROJECT_PATH/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh &
+# run cellranger aggr on the ATAC samples
+cd $PROJECT_PATH/pipeline/ATAC.ASAP && $PROJECT_PATH/pipeline/ATAC.ASAP/run_cellranger.aggr_atac.sh &
+wait
+
+# load RNA cellranger matrices into Seurat and generate a demultiplexed raw Seurat object
+cd $PROJECT_PATH/analysis/RNA.FB.VDJ && Rscript $PROJECT_PATH/analysis/run_seurat.demux_rna.R &
+# load ATAC cellranger/kite matrices into Seurat and generate a demultiplexed raw Seurat object
+cd $PROJECT_PATH/analysis/ATAC.ASAP && Rscript $PROJECT_PATH/analysis/run_seurat.demux_atac.R &
