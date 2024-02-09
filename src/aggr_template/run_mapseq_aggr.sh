8b1402d6dc3d8f56637100f5d52b27f3692ccdbf
willisbillis <elliott.williams14@gmail.com>
2024-02-09 10:30:56 -0500

    fix paths

--- src/aggr_template/run_mapseq_aggr.sh
+++ src/aggr_template/run_mapseq_aggr.sh
@@ -15,12 +15,12 @@ source ./project_config.txt
 # TODO: add unit tests here
 
 # run cellranger aggr on the RNA+FB samples
-cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB && $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB/run_cellranger.aggr_rna.sh &
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ && $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh &
 # run cellranger aggr on the ATAC samples
 cd $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP && $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP/run_cellranger.aggr_atac.sh &
 wait
 
 # load RNA cellranger matrices into Seurat and generate a demultiplexed raw Seurat object
-cd $PROJECT_PATH/$PROJECT_NAME/analysis/RNA.FB && Rscript $PROJECT_PATH/$PROJECT_NAME/analysis/run_seurat.demux_rna.R &
+cd $PROJECT_PATH/$PROJECT_NAME/analysis/RNA.FB.VDJ && Rscript $PROJECT_PATH/$PROJECT_NAME/analysis/RNA.FB.VDJ/run_seurat.demux_rna.R &
 # load ATAC cellranger/kite matrices into Seurat and generate a demultiplexed raw Seurat object
-cd $PROJECT_PATH/$PROJECT_NAME/analysis/ATAC.ASAP && Rscript $PROJECT_PATH/$PROJECT_NAME/analysis/run_seurat.demux_atac.R &
+cd $PROJECT_PATH/$PROJECT_NAME/analysis/ATAC.ASAP && Rscript $PROJECT_PATH/$PROJECT_NAME/analysis/ATAC.ASAP/run_seurat.demux_atac.R &
