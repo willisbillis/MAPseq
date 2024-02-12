36ba10918f12337e053cf031f661abedb80a0d86
willisbillis <elliott.williams14@gmail.com>
2024-02-12 10:51:55 -0500

    account for multi runs

--- src/aggr_template/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh
+++ src/aggr_template/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh
@@ -27,7 +27,11 @@ echo "$(date) Running Cell Ranger version $CR_version using binary $(which cellr
 printf '%s\n' sample_id molecule_h5 | paste -sd ',' >> $AGGR_CSV
 for sample_path in "${SAMPLES_ARRAY[@]}"; do
   sample_name=$(basename $sample_path)
-  count_molecule_file=${sample_path}outs/molecule_info.h5
+  if [ -d "${sample_path}outs/multi" ]; then
+    count_molecule_file=${sample_path}outs/multi/raw_molecule_info.h5
+  else
+    count_molecule_file=${sample_path}outs/molecule_info.h5
+  fi
   printf '%s\n' $sample_name $count_molecule_file | paste -sd ',' >> $AGGR_CSV
 done
 
