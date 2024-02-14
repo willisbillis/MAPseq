288c202917cb3b2a7b1b9f47a58174a3ed2972c2
willisbillis <elliott.williams14@gmail.com>
2024-02-13 19:02:31 -0500

    paths

--- src/aggr_template/pipeline/ATAC.ASAP/run_seurat.demux_atac.R
+++ src/aggr_template/pipeline/ATAC.ASAP/run_seurat.demux_atac.R
@@ -41,7 +41,7 @@ ASAP_NAMING_ID = Sys.getenv("ASAP_NAMING_ID")[1]
 # Set all the local variables for this pipeline
 HTO_DEMUX_PATH = paste0(PROJECT_PATH,"/",PROJECT_NAME,"/pipeline/ATAC.ASAP/hashtag_ref_atac.csv")
 OUTS_DIR = paste0(PROJECT_PATH,"/",PROJECT_NAME,"/pipeline/ATAC.ASAP/ATAC/",PROJECT_NAME,"_aggr/outs")
-OUTPUT_DIR = paste0(PROJECT_PATH,"/",PROJECT_NAME,"/analysis/ATAC.ASAP/data")
+OUTPUT_DIR = paste0(PROJECT_PATH,"/",PROJECT_NAME,"/analysis/ATAC.ASAP")
 ################################################################################
 dir.create(OUTPUT_DIR, showWarnings = F, recursive = T)
 
