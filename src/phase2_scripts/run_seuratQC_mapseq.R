14f26a5eaf6e00cf19e1e19700b98b2a4f98ee68
willisbillis <elliott.williams14@gmail.com>
2024-02-15 15:15:34 -0500

    better hto figures, filtering, and now deg, clusters, and umaps

--- src/phase2_scripts/run_seuratQC_mapseq.R
+++ src/phase2_scripts/run_seuratQC_mapseq.R
@@ -67,42 +67,53 @@ p = RidgePlot(sc_total,
               features = rownames(sc_total[["HTO"]]),
               ncol = ncol,
               group.by = "hash.ID")
-ggsave(paste0(PROJECT_DIR, "/ridgeplot_called_", PROJECT_NAME, ".png"),
+ggsave(paste0("ridgeplot_called_", PROJECT_NAME, ".png"),
        p, height = OUTPUT_FIG_HEIGHT, width = OUTPUT_FIG_WIDTH * floor(ncol*0.5))
 
 p = RidgePlot(sc_total,
               features = rownames(sc_total[["HTO"]]),
               ncol = ncol,
               group.by = "HTO_maxID")
-ggsave(paste0(PROJECT_DIR, "/ridgeplot_max_", PROJECT_NAME, ".png"),
+ggsave(paste0("ridgeplot_max_", PROJECT_NAME, ".png"),
        p, height = OUTPUT_FIG_HEIGHT, width = OUTPUT_FIG_WIDTH * floor(ncol*0.5))
 
 p = RidgePlot(sc_total,
               features = rownames(sc_total[["HTO"]]),
               ncol = ncol,
               group.by = "HTO_classification.global")
-ggsave(paste0(PROJECT_DIR, "/ridgeplot_classification_", PROJECT_NAME, ".png"),
+ggsave(paste0("ridgeplot_classification_", PROJECT_NAME, ".png"),
        p, height = OUTPUT_FIG_HEIGHT, width = OUTPUT_FIG_WIDTH * floor(ncol*0.5))
 
-confusion_matrix = as.data.frame(table(sc_total$HTO_maxID, sc_total$HTO_secondID))
-confusion_matrix = confusion_matrix[!(confusion_matrix$Var1 == confusion_matrix$Var2), ]
-confusion_matrix = confusion_matrix[confusion_matrix$Freq != 0, ]
-confusion_matrix$Var1 = as.character(confusion_matrix$Var1)
-confusion_matrix$Var2 = as.character(confusion_matrix$Var2)
+top_confusion_matrix = as.data.frame(table(sc_total$HTO_maxID[sc_total$HTO_margin > quantile(sc_total$HTO_margin, 0.95)],
+       sc_total$HTO_secondID[sc_total$HTO_margin > quantile(sc_total$HTO_margin, 0.95)]))
+bot_confusion_matrix = as.data.frame(table(sc_total$HTO_maxID[sc_total$HTO_margin < quantile(sc_total$HTO_margin, 0.05)],
+       sc_total$HTO_secondID[sc_total$HTO_margin < quantile(sc_total$HTO_margin, 0.05)]))
 
-min_idx = as.numeric(rownames(confusion_matrix)[confusion_matrix$Freq == min(confusion_matrix$Freq)][1])
-max_idx = as.numeric(rownames(confusion_matrix)[confusion_matrix$Freq == max(confusion_matrix$Freq)])
+top_confusion_matrix$Var1 = as.character(top_confusion_matrix$Var1)
+top_confusion_matrix$Var2 = as.character(top_confusion_matrix$Var2)
+top_confusion_matrix = top_confusion_matrix[!(top_confusion_matrix$Var1 == top_confusion_matrix$Var2), ]
+top_confusion_matrix = top_confusion_matrix[top_confusion_matrix$Freq != 0, ]
+
+bot_confusion_matrix$Var1 = as.character(bot_confusion_matrix$Var1)
+bot_confusion_matrix$Var2 = as.character(bot_confusion_matrix$Var2)
+bot_confusion_matrix = bot_confusion_matrix[!(bot_confusion_matrix$Var1 == bot_confusion_matrix$Var2), ]
+bot_confusion_matrix = bot_confusion_matrix[bot_confusion_matrix$Freq != 0, ]
+
+top_confusion_matrix = top_confusion_matrix[order(top_confusion_matrix$Freq),]
+bot_confusion_matrix = bot_confusion_matrix[order(-bot_confusion_matrix$Freq),]
+best_htos = c(top_confusion_matrix$Var1[1], top_confusion_matrix$Var2[1])
+worst_htos = c(bot_confusion_matrix$Var1[1], bot_confusion_matrix$Var2[1])
 
 Idents(sc_total) = "hash.ID"
 
-p = FeatureScatter(sc_total,
-    feature1 = confusion_matrix$Var1[rownames(confusion_matrix) == min_idx], feature2 = confusion_matrix$Var2[rownames(confusion_matrix) == min_idx])
-ggsave(paste0(PROJECT_DIR, "/scatter_good.hto.separation_", PROJECT_NAME, ".png"),
+p = FeatureScatter(sc_total, cells = colnames(sc_total)[sc_total$hash.ID %in% c(best_htos, "Doublet")],
+    feature1 = best_htos[1], feature2 = best_htos[2])
+ggsave(paste0("scatter_best.hto.separation_", PROJECT_NAME, ".png"),
        p, height = OUTPUT_FIG_HEIGHT, width = OUTPUT_FIG_WIDTH)
 
-p = FeatureScatter(sc_total,
-    feature1 = confusion_matrix$Var1[rownames(confusion_matrix) == max_idx], feature2 = confusion_matrix$Var2[rownames(confusion_matrix) == max_idx])
-ggsave(paste0(PROJECT_DIR, "/scatter_worst.hto.separation_", PROJECT_NAME, ".png"),
+p = FeatureScatter(sc_total, cells = colnames(sc_total)[sc_total$hash.ID %in% c(worst_htos, "Doublet")],
+    feature1 = worst_htos[1], feature2 = worst_htos[2])
+ggsave(paste0("scatter_worst.hto.separation_", PROJECT_NAME, ".png"),
        p, height = OUTPUT_FIG_HEIGHT, width = OUTPUT_FIG_WIDTH)
 
 sc_total = subset(sc_total, HTO_classification.global == "Singlet")
@@ -113,12 +124,12 @@ sc_total[["percent.Ig"]] <- PercentageFeatureSet(sc_total, pattern = igs)
 sc_total[["percent.mt"]] <- PercentageFeatureSet(sc_total, pattern = mt_pattern)
 
 p = DensityScatter(sc_total, "percent.Ig", "percent.mt", quantiles=TRUE)
-ggsave(paste0(PROJECT_DIR, "/scatter_pct.Ig.v.pct.mt_alldata.png"),
+ggsave("scatter_pct.Ig.v.pct.mt_alldata.png",
        p, width = OUTPUT_FIG_WIDTH, height = OUTPUT_FIG_HEIGHT)
 
 p = DensityScatter(sc_total, "nCount_RNA", "nFeature_RNA",
                    quantiles = TRUE, log_x = TRUE, log_y = TRUE)
-ggsave(paste0(PROJECT_DIR, "/scatter_nCountRNA.v.nFeatRNA_alldata.png"),
+ggsave("scatter_nCountRNA.v.nFeatRNA_alldata.png",
        p, width = OUTPUT_FIG_WIDTH, height = OUTPUT_FIG_HEIGHT)
 ###############################################################################
 ###############################################################################
@@ -131,23 +142,17 @@ p = DensityScatter(sc_total, "nFeature_RNA", "percent.mt", quantiles=TRUE, log_x
 nFeature_mt_plot <- p +
   geom_hline(yintercept=MAX_PCT_MT,linetype='dashed') +
   geom_vline(xintercept=c(MIN_GENE_READS, MAX_GENE_READS), linetype='dashed')
-ggsave(paste0(PROJECT_DIR, "/scatter_nFeatRNA.v.MT_alldata.png"), nFeature_mt_plot,
+ggsave("scatter_nFeatRNA.v.MT_alldata.png", nFeature_mt_plot,
        width=OUTPUT_FIG_WIDTH, height=OUTPUT_FIG_HEIGHT)
 ###############################################################################
 ###############################################################################
-categorize_cells = function(sc_seurat, cat_column, names_prefix) {
-  table(sc_seurat$patient_id, unlist(sc_seurat[[cat_column]]), dnn = list("Patients", cat_column)) %>%
-    data.frame %>%
-    pivot_wider(names_from = all_of(cat_column), values_from = "Freq", names_prefix = names_prefix) %>%
-    select(!Patients)
-}
+# Filtering of cells based on QC criteria
 
 # Expression Filter
 stats = data.frame(table(sc_total$patient_id))
 colnames(stats) = c("Patients", "Unfiltered_Cells")
 rownames(stats) = stats$Patients
-stats$Unfiltered_Average_Expression = AggregateExpression(sc_total, group.by="patient_id")$RNA %>% colSums / stats$Unfiltered_Cells
-stats = cbind(stats, categorize_cells(sc_total, "hash.ID", "Unfiltered_"))
+stats$Unfiltered_Average_Expression = round(AggregateExpression(sc_total, group.by="patient_id")$RNA %>% colSums / stats$Unfiltered_Cells)
 
 stats$Filtered_by_MT_NF = table(sc_total$patient_id, sc_total$nFeature_RNA <= MIN_GENE_READS & sc_total$percent.mt >= MAX_PCT_MT)[, "TRUE"]
 stats$Filtered_by_MT = table(sc_total$patient_id, sc_total$nFeature_RNA <= MIN_GENE_READS)[, "TRUE"] - stats$Filtered_by_MT_NF
@@ -158,14 +163,90 @@ sc = subset(sc_total,
                    nFeature_RNA > MIN_GENE_READS &
                    nFeature_RNA < MAX_GENE_READS)
 
-stats$Filtered_Cells = as.numeric(table(sc$patient_id))
-stats = cbind(stats, categorize_cells(sc_total, "hash.ID", "Filtered_"))
-
 # HTO Filter
 sc <- subset(sc, subset = HTO_classification.global == "Singlet")
 sc$hash.ID = factor(sc$hash.ID, levels = unique(sc$hash.ID)) # Remove Doublet and Negative factors
 
-stats$Filtered_Singleton_Cells = as.numeric(table(sc$patient_id))
-stats$Filtered_Singleton_Average_Expression = AggregateExpression(sc, group.by="patient_id")$RNA %>% colSums %>% round
+stats$Filtered_Cells = as.numeric(table(sc$patient_id))
+stats$Filtered_Average_Expression = round(AggregateExpression(sc, group.by="patient_id")$RNA %>% colSums / stats$Filtered_Cells)
 
 write.csv(stats, "table_patientID_filtering.stats.csv", quote = F, row.names = F)
+###############################################################################
+# Clustering and DEG (differentially expressed genes) for ADT assay
+DefaultAssay(sc) = "RNA"
+sc = SCTransform(sc)
+
+# Filter out Ig genes from VariableFeatures, they will clog the results as they are highly-variable by nature
+VariableFeatures(sc) = VariableFeatures(sc)[!grepl(igs, VariableFeatures(sc))]
+
+sc = RunPCA(sc, features = VariableFeatures(sc), reduction.name = "rna_pca", reduction.key = "rnapca_", verbose = FALSE)
+sc = RunUMAP(sc, reduction = 'rna_pca', dims = 1:40, reduction.key = "rnaumap_", verbose = FALSE)
+
+sc <- FindNeighbors(sc, dims = 1:40, reduction = 'rna_pca', verbose = FALSE)
+for (res in c(1, 0.5, 0.25, 0.1, 0.05)) {
+  sc = FindClusters(sc, resolution = res, algorithm = 3, verbose = FALSE)
+}
+
+p = clustree(sc, prefix="SCT_snn_res.")
+ggsave("clustree_clusters_rna.png", p,
+       width=OUTPUT_FIG_WIDTH, height=OUTPUT_FIG_HEIGHT)
+
+sc$seurat_clusters_rna = sc[[paste0("SCT_snn_res.", 0.25)]]
+Idents(sc) = "seurat_clusters_rna"
+sc$seurat_clusters = NULL
+sc$seurat_clusters_rna = factor(sc$seurat_clusters_rna)
+
+DefaultAssay(sc) = "RNA"
+all_markers = FindAllMarkers(sc)
+all_markers = all_markers[all_markers$p_val_adj < 0.05,]
+fwrite(all_markers, "DEG_clusters.res0.25.csv", row.names = F, quote = F)
+
+rna_umap = DimPlot(sc, reduction = "umap", group.by = "seurat_clusters_rna")
+###############################################################################
+# Clustering and DEP (differentially expressed proteins) for ADT assay
+DefaultAssay(sc) = "ADT"
+
+sc = NormalizeData(sc)
+sc <- ScaleData(sc, features = rownames(sc), verbose = FALSE)
+sc <- RunPCA(sc, features = rownames(sc), approx = FALSE, reduction.name = "adt_pca", reduction.key = "adtpca_", verbose = FALSE)
+sc = RunUMAP(sc, reduction = 'adt_pca', dims = 1:8, reduction.key = "adtumap_", verbose = FALSE)
+
+sc <- FindNeighbors(sc, dims = 1:8, reduction = 'adt_pca', verbose = FALSE)
+for (res in c(1, 0.5, 0.25, 0.1, 0.05)) {
+  sc = FindClusters(sc, resolution = res, algorithm = 3, verbose = FALSE)
+}
+
+p = clustree(sc, prefix="ADT_snn_res.")
+ggsave("clustree_clusters_adt.png", p,
+       width=OUTPUT_FIG_WIDTH, height=OUTPUT_FIG_HEIGHT)
+
+sc$seurat_clusters_adt = sc[[paste0("ADT_snn_res.", 0.25)]]
+Idents(sc) = "seurat_clusters_adt"
+sc$seurat_clusters = NULL
+sc$seurat_clusters_adt = factor(sc$seurat_clusters_adt)
+
+all_markers = FindAllMarkers(sc)
+all_markers = all_markers[all_markers$p_val_adj < 0.05,]
+fwrite(all_markers, "DEP_clusters.res0.25.csv", row.names = F, quote = F)
+
+adt_umap = DimPlot(sc, reduction = "umap", group.by = "seurat_clusters_adt")
+###############################################################################
+# Creating RNA and ADT UMAP figures
+p = rna_umap + adt_umap
+ggsave("umap_rna.adt_clusters.png", p, width = OUTPUT_FIG_WIDTH * 2, height = OUTPUT_FIG_HEIGHT)
+
+# save Seurat object
+saveRDS(sc, paste0(PROJECT_DIR,"/data/qc_rna.hto.adt_", PROJECT_NAME, ".RDS"))
+###############################################################################
+###############################################################################
+# create ShinyCell app with data - MUST pre-authenticate using shinyapps.io
+#      token with rsconnect
+if (FALSE) {
+  sc = AddMetaData(sc, t(LayerData(sc, assay="HTO")))
+  sc = AddMetaData(sc, t(LayerData(sc, assay="ADT")))
+  sc_conf = createConfig(sc)
+  makeShinyApp(sc, sc_conf, gene.mapping = TRUE,
+               shiny.title = paste0(PROJECT_NAME, " RNA + HTO + ADT"),
+               shiny.dir = paste0("shiny_",PROJECT_NAME,"_rna"),
+               gex.assay="RNA")
+}
