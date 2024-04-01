6b45999d5aba054f3c7e71b094ac7e3f911413d7
willisbillis <elliott.williams14@gmail.com>
2024-04-01 16:48:51 -0400

    fix stderr redirect

--- src/mapseq_template/run_mapseq.sh
+++ src/mapseq_template/run_mapseq.sh
@@ -18,8 +18,8 @@ source ./project_config.txt
 # compile reports from each step
 mkdir -p $PROJECT_PATH/reports
 
-rna_fqs=$(ls $RNA_DIR/*fastq.gz 2> /dev/null)
-atac_fqs=$(ls $ATAC_DIR/*fastq.gz 2> /dev/null)
+rna_fqs=$(ls $RNA_DIR/*fastq.gz 2>/dev/null)
+atac_fqs=$(ls $ATAC_DIR/*fastq.gz 2>/dev/null)
 
 if [ $(wc -c <<< $rna_fqs) == 1 ] && [ $(wc -c <<< $atac_fqs) == 1 ]; then
     # demultiplex any fastqs available on RNA.FB.VDJ or ATAC.ASAP side
@@ -29,7 +29,7 @@ fi
 
 # mv the fastqs from the data directory
 RNA_FASTQ_PATH=$PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs
-stored_rna_fqs=$(ls $RNA_FASTQ_PATH/*gz 2> /dev/null)
+stored_rna_fqs=$(ls $RNA_FASTQ_PATH/*gz 2>/dev/null)
 if [ $(wc -c <<< $rna_fqs) > 1 ] && [ $(wc -c <<< $stored_rna_fqs) == 1 ]; then
     mkdir -p $RNA_FASTQ_PATH
     rna_samples=$(ls $RNA_DIR/*fastq.gz)
@@ -38,7 +38,7 @@ if [ $(wc -c <<< $rna_fqs) > 1 ] && [ $(wc -c <<< $stored_rna_fqs) == 1 ]; then
     done
 fi
 ATAC_FASTQ_PATH=$PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs
-stored_atac_fqs=$(ls $ATAC_FASTQ_PATH/*gz 2> /dev/null)
+stored_atac_fqs=$(ls $ATAC_FASTQ_PATH/*gz 2>/dev/null)
 if [ $(wc -c <<< $atac_fqs) > 1 ] && [ $(wc -c <<< $stored_atac_fqs) == 1 ]; then
     mkdir -p $ATAC_FASTQ_PATH
     atac_samples=($(ls $ATAC_DIR/*fastq.gz))
@@ -48,14 +48,14 @@ if [ $(wc -c <<< $atac_fqs) > 1 ] && [ $(wc -c <<< $stored_atac_fqs) == 1 ]; the
 fi
 
 # check for any fastqs from RNA.FB.VDJ
-rna_fqs=$(ls $PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs/*fastq.gz 2> /dev/null)
+rna_fqs=$(ls $PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs/*fastq.gz 2>/dev/null)
 if [ $(wc -c <<< $rna_fqs) > 1 ]; then
     cd $PROJECT_PATH/pipeline/RNA.FB.VDJ && $PROJECT_PATH/pipeline/RNA.FB.VDJ/run_cellranger_RNA.FB.VDJ.sh &&
         cp $PROJECT_PATH/pipeline/RNA.FB.VDJ/reports/* $PROJECT_PATH/reports
 fi
 
 # check for any fastqs from ATAC.ASAP
-atac_fqs=$(ls $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/*fastq.gz 2> /dev/null)
+atac_fqs=$(ls $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/*fastq.gz 2>/dev/null)
 if [ $(wc -c <<< $atac_fqs) > 1 ]; then
     cd $PROJECT_PATH/pipeline/ATAC.ASAP
     $PROJECT_PATH/pipeline/ATAC.ASAP/run_asap_to_kite.sh && \
