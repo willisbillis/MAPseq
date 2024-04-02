87658b9985aa6b1f8249afd6c3e67d84b9cde3f5
willisbillis <elliott.williams14@gmail.com>
2024-04-02 16:22:54 -0400

    try to capture all asap and atac fqs

--- src/mapseq_template/data/run_mkfastq.sh
+++ src/mapseq_template/data/run_mkfastq.sh
@@ -30,12 +30,12 @@ if [ $(wc -l < ${PROJECT_NAME}.ATAC.sampleManifest.csv) -gt 1 ] && [ $(wc -c <<<
         --delete-undetermined \
         --localcores=$NCPU --localmem=$MEM
 
-
     ATAC_FC_PATH=$(ls -d $OUTPUT_DIR/${PROJECT_NAME}_ATAC/outs/fastq_path/*/ | grep -v "Reports\|Stats")
     ATAC_FLOWCELL_ID=$(basename $ATAC_FC_PATH)
     # Standardize where fastqs live between received FQs and non-demuxed FQs
     NEW_FQ_PATH=$PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs
-    mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/$ATAC_FLOWCELL_ID/* $NEW_FQ_PATH
+    mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/$ATAC_FLOWCELL_ID/*/*.gz $NEW_FQ_PATH
+    mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/$ATAC_FLOWCELL_ID/*.gz $NEW_FQ_PATH
     # rearrange mkfastq outputs
     mkdir -p $NEW_FQ_PATH/mkfastq_outputs
     mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/* $NEW_FQ_PATH/mkfastq_outputs
@@ -60,7 +60,7 @@ if [ $(wc -l < ${PROJECT_NAME}.RNA.sampleManifest.csv) -gt 1 ] && [ $(wc -c <<< 
     RNA_FLOWCELL_ID=$(basename $RNA_FC_PATH)
     # Standardize where fastqs live between received FQs and non-demuxed FQs
     NEW_FQ_PATH=$PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs
-    mv $PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs/fastq_path/$RNA_FLOWCELL_ID/* $NEW_FQ_PATH
+    mv $PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs/fastq_path/$RNA_FLOWCELL_ID/*/*.gz $NEW_FQ_PATH
     # rearrange mkfastq outputs
     mkdir -p $NEW_FQ_PATH/mkfastq_outputs
     mv $PROJECT_PATH/data/${PROJECT_NAME}_RNA/outs/fastq_path/* $NEW_FQ_PATH/mkfastq_outputs
