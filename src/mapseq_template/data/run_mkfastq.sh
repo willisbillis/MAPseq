4d50325ec238d0b0d7629a8be6bbeec776ec8cb1
willisbillis <elliott.williams14@gmail.com>
2024-04-03 11:31:47 -0400

    got the paths right this time

--- src/mapseq_template/data/run_mkfastq.sh
+++ src/mapseq_template/data/run_mkfastq.sh
@@ -34,7 +34,7 @@ if [ $(wc -l < ${PROJECT_NAME}.ATAC.sampleManifest.csv) -gt 1 ] && [ $(wc -c <<<
     ATAC_FLOWCELL_ID=$(basename $ATAC_FC_PATH)
     # Standardize where fastqs live between received FQs and non-demuxed FQs
     NEW_FQ_PATH=$PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs
-    mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/$ATAC_FLOWCELL_ID/*.gz $NEW_FQ_PATH
+    mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/$ATAC_FLOWCELL_ID/*/*.gz $NEW_FQ_PATH
     mv $PROJECT_PATH/data/${PROJECT_NAME}_ATAC/outs/fastq_path/*.gz $NEW_FQ_PATH
     # rearrange mkfastq outputs
     mkdir -p $NEW_FQ_PATH/mkfastq_outputs
