7ae37e3b1b106fb84b9556d2c4e009458678d72d
willisbillis <elliott.williams14@gmail.com>
2024-04-03 12:20:27 -0400

    make reports dir for atac

--- src/mapseq_template/pipeline/ATAC.ASAP/run_cellranger_ATAC.sh
+++ src/mapseq_template/pipeline/ATAC.ASAP/run_cellranger_ATAC.sh
@@ -18,6 +18,8 @@ OUTPUT_FILE=$OUTPUT_DIR/cellranger_atac_mapping.log
 ################################################################################
 mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR/reports
+
 sample_name_col=$(cut -d, -f2 $PROJECT_PATH/data/${PROJECT_NAME}.ATAC.sampleManifest.csv)
 sample_names=$(printf -- '%s ' "${sample_name_col[@]}" | grep -v Sample | uniq)
 atac_samples=($(printf -- '%s ' "${sample_names[@]}" | grep .*${ATAC_NAMING_ID}.*))
@@ -34,5 +36,5 @@ for sample in "${atac_samples[@]}"; do
     --reference=$ATAC_REF_PATH --fastqs=$FASTQ_PATH \
     --localcores=$NCPU --localmem=$MEM
 
-  cp $sample/outs/web_summary.html $OUTPUT_DIR/reports/mapping.report_${sample}.html
+  cp $OUTPUT_DIR/$sample/outs/web_summary.html $OUTPUT_DIR/reports/mapping.report_${sample}.html
 done
