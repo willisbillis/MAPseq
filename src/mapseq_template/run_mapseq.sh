05c72d4dccb88e65350198769eea023cc3c5f6e0
willisbillis <elliott.williams14@gmail.com>
2024-05-15 09:29:29 -0400

    fix preflight checks path

--- src/mapseq_template/run_mapseq.sh
+++ src/mapseq_template/run_mapseq.sh
@@ -14,7 +14,7 @@
 source ./project_config.txt
 ################################################################################
 echo "Performing pre-flight checks..."
-./pipeline/preflight_checks.sh
+$PROJECT_PATH/pipeline/preflight_checks.sh
 
 # create directory to compile reports from each step
 mkdir -p $PROJECT_PATH/reports
