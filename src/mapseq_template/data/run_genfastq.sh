90054a8c3907494bea2e47675c1c50ed8ca598a0
willisbillis <elliott.williams14@gmail.com>
2024-05-16 13:02:07 -0400

    update arr decl

--- src/mapseq_template/data/run_genfastq.sh
+++ src/mapseq_template/data/run_genfastq.sh
@@ -17,11 +17,11 @@ OUTPUT_FILE=$OUTPUT_DIR/cellranger_mkfastq.log
 cd $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR/reports
 
-rna_fqs=$(ls $RNA_DIR/*fastq.gz 2>/dev/null)
-atac_fqs=$(ls $ATAC_DIR/*fastq.gz 2>/dev/null)
+declare -a rna_fqs=$(ls $RNA_DIR/*fastq.gz 2>/dev/null)
+declare -a atac_fqs=$(ls $ATAC_DIR/*fastq.gz 2>/dev/null)
 
 ## ATAC.ASAP fastq generation
-if [[ $(wc -l < ${PROJECT_NAME}.ATAC.sampleManifest.csv) -gt 1 ]] && [[ $(wc -c <<< $atac_fqs) -eq 1 ]]; then
+if [[ $(wc -l < ${PROJECT_NAME}.ATAC.sampleManifest.csv) -gt 1 ]] && [[ ${#atac_fqs[@]} -gt 0 ]]; then
     # Need to demultiplex fastqs from bcl files, run cellranger mkfastq
     CR_atac_version=$(cellranger-atac --version | grep -Po '(?<=cellranger-atac-)[^;]+')
     echo "$(date) Running Cell Ranger ATAC version $CR_atac_version using binary $(which cellranger-atac)" >> $OUTPUT_FILE
@@ -58,7 +58,7 @@ elif [[ $(wc -l < ${PROJECT_NAME}.ATAC.sampleManifest.csv) -gt 1 ]]; then
 fi
 
 ## RNA.FB.BCR fastq generation
-if [ $(wc -l < ${PROJECT_NAME}.RNA.sampleManifest.csv) -gt 1 ] && [ $(wc -c <<< $rna_fqs) -eq 1 ]; then
+if [ $(wc -l < ${PROJECT_NAME}.RNA.sampleManifest.csv) -gt 1 ] && [ ${#rna_fqs[@]} -gt 0 ]; then
     # Need to demultiplex fastqs from bcl files, run cellranger mkfastq
     CR_version=$(cellranger --version | grep -Po '(?<=cellranger-)[^;]+')
     echo "$(date) Running Cell Ranger version $CR_version using binary $(which cellranger)" >> $OUTPUT_FILE
