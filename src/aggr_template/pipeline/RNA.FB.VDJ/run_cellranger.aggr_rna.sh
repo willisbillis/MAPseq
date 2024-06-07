b90a188702b266c79565c74339039e719b104fc1
willisbillis <elliott.williams14@gmail.com>
2024-06-07 10:04:09 -0400

    comment out cellbender for now

--- src/aggr_template/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh
+++ src/aggr_template/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh
@@ -48,5 +48,5 @@ cellranger aggr --id=${PROJECT_NAME}_aggr \
 	--csv $AGGR_CSV  --normalize none \
 	--localcores=$NCPU --localmem=$MEM
 
-cellbender remove-background --input $OUTPUT_DIR/${PROJECT_NAME}_aggr/outs/count/filtered_feature_bc_matrix.h5 \
-  --output $OUTPUT_DIR/${PROJECT_NAME}_aggr/outs/count/cellbender_feature_bc_matrix.h5
+#cellbender remove-background --input $OUTPUT_DIR/${PROJECT_NAME}_aggr/outs/count/filtered_feature_bc_matrix.h5 \
+#  --output $OUTPUT_DIR/${PROJECT_NAME}_aggr/outs/count/cellbender_feature_bc_matrix.h5
