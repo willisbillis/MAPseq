3f30d0d65400a4abd1840ddf788e69cbb2e41588
willisbillis <20462546+willisbillis@users.noreply.github.com>
2024-10-09 14:59:07 -0400

    special character adjustment

--- src/aggr_template/pipeline/ATAC.ASAP/run_seurat.demux_atac.R
+++ src/aggr_template/pipeline/ATAC.ASAP/run_seurat.demux_atac.R
@@ -181,9 +181,9 @@ for (idx in seq_len(nrow(metadata_df))) {
           if ("unique_sample_id" %in% colnames(combined_data)) {
             combined_data$unique_sample_id = paste0(combined_data$unique_sample_id,
                                                     "-",
-                                                    gsub("-", ".+.", combined_data[[metadata_col]]))
+                                                    gsub("-", ".\\+.", combined_data[[metadata_col]]))
           } else {
-            combined_data$unique_sample_id = gsub("-", ".+.", combined_data[[metadata_col]])
+            combined_data$unique_sample_id = gsub("-", ".\\+.", combined_data[[metadata_col]])
           }
           extra_metadata = c(extra_metadata, metadata_col)
         }
@@ -287,7 +287,7 @@ for (idx in seq_len(nrow(metadata_df))) {
         for (metadata_col in extra_metadata) {
           for (geno_cl in unique(cluster_mapping$cluster)) {
             mask = (hashtag$genotype_cluster == geno_cl) & (is.na(hashtag@meta.data[[metadata_col]]))
-            hashtag@meta.data[[metadata_col]][mask] = gsub(".+.", "-",
+            hashtag@meta.data[[metadata_col]][mask] = gsub(".\\+.", "-",
                                                            cluster_mapping[[metadata_col]][cluster_mapping$cluster == geno_cl])
           }
         }
