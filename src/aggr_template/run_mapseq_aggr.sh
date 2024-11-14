683e5dc5d34c72793ff6decbe7aedab69cfca772
willisbillis <20462546+willisbillis@users.noreply.github.com>
2024-11-14 12:04:22 -0500

    add souporcell genotype demux step to aggr

--- src/aggr_template/run_mapseq_aggr.sh
+++ src/aggr_template/run_mapseq_aggr.sh
@@ -22,12 +22,21 @@ export ASAP_NAMING_ID=$ASAP_NAMING_ID
 # TODO: add unit tests here
 
 # run cellranger aggr on the RNA+FB samples
-cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ && $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ/run_cellranger.aggr_rna.sh &
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ && ./run_cellranger.aggr_rna.sh &
 # run cellranger aggr on the ATAC samples
-cd $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP && $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP/run_cellranger.aggr_atac.sh &
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP && ./run_cellranger.aggr_atac.sh &
 wait
 
+# activate souporcell conda environment
+conda activate souporcell
+# run souporcell genotype demultiplexing for RNA samples
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ && ./run_souporcell.demux_rna.sh
+# run souporcell genotype demultiplexing for ATAC samples
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP && ./run_souporcell.demux_atac.sh
+# deactivate souporcell conda environment
+conda deactivate
+
 # load RNA cellranger matrices into Seurat and generate a demultiplexed raw Seurat object
-cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ && Rscript $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ/run_seurat.demux_rna.R
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/RNA.FB.VDJ && Rscript run_seurat.demux_rna.R
 # load ATAC cellranger/kite matrices into Seurat and generate a demultiplexed raw Seurat object
-cd $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP && Rscript $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP/run_seurat.demux_atac.R
+cd $PROJECT_PATH/$PROJECT_NAME/pipeline/ATAC.ASAP && Rscript run_seurat.demux_atac.R
